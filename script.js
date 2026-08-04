2f45a2ada864f57764916f85c414daa85109978d
lethiciasantosribeiro-droid <lethicia.santos.ribeiro@escola.pr.gov.br>
2026-08-04 10:07:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const botoes = document.querySelectorAll("button");
 
+botoes.forEach(function (botao) {
+    let curtiu = false;
+    botao.addEventListener("click", botaoClicado);
+
+    function botaoClicado() {
+        let texto = botao.querySelector("span");
+        if (curtiu === false) {
+            texto.textContent++;
+            curtiu = true;
+            botao.style.backgroundColor = "#ffe6e6"; // Feedback visual ao clicar
+        } else {
+            texto.textContent--;
+            curtiu = false;
+            botao.style.backgroundColor = "transparent";
+        }
+    }
+})
